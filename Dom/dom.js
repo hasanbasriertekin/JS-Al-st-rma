94f8d821bc0f3c7766854184d5b9df4a53d03a86
Hasan Basri ERTEKİN <hasanbasriertekin96@gmail.com>
2024-08-29 00:15:42 +0300

    update

--- Dom/dom.js
+++ Dom/dom.js
@@ -1,3 +1,3 @@
 //index.html dosyamızda bir div elementimiz bulunmaktadır. Bu elementi dom.js dosyamızda çağıralım 
 // ip ucu : document.getElementBy... gibi
-//önce script dosyamızı bağlarsak hata alırız tabiki heycan yapma:)
+//önce script dosyamızı bağlamazsak hata alırız tabiki heycan yapma:)
